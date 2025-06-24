2d2be0375c5655067f52cccec866a537aad07418
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-24 11:01:13 +0100

    feat: update Legal page layout and content

--- src/app/legal/page.tsx
+++ src/app/legal/page.tsx
@@ -1,27 +1,12 @@
-import Image from "next/image";
-
 export default function Legal() {
   return (
-    <main>
-      <h1>Mentions Légales</h1>
+    <main className="main">
+      <h1 className="title">Mentions Légales</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-        tempora eligendi ea itaque minima sint dolorem ad modi, quos dolorum sit
-        eius adipisci? Corporis optio, rerum expedita omnis atque unde!
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
-        perspiciatis aliquid voluptates? Molestiae rem perferendis neque tenetur
-        explicabo qui reiciendis sed saepe quia. Corporis debitis harum quam
-        facere delectus! Debitis tempora voluptates accusamus blanditiis soluta
-        vel eligendi ab. Numquam adipisci odit tempora tempore, cupiditate
-        mollitia unde possimus. Ad est porro commodi officia magnam beatae,
-        totam illum non libero optio quam atque, pariatur adipisci sunt odio
-        minus? Esse, architecto? Ut perferendis tenetur praesentium soluta
-        beatae qui consequatur accusamus cupiditate voluptate corrupti, animi
-        odit quod. Exercitationem repudiandae facere magni blanditiis minima
-        consectetur doloribus, voluptas adipisci facilis veniam, mollitia sed
-        ducimus ipsum quae!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+        quas necessitatibus. Nostrum tempora aperiam sunt ipsum quibusdam
+        tenetur, delectus ex nihil beatae animi vero explicabo, alias dolores
+        quidem nemo deserunt!
       </p>
     </main>
   );
