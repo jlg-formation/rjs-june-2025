639e1a2d5e48519c3baa362e60dd588a1047761f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-24 16:10:14 +0100

    feat: Initialize project structure with global styles, layout, and routing
    - Added global CSS styles with Tailwind CSS integration.
    - Created RootLayout component for consistent page structure.
    - Implemented Legal page with a real-time clock and counter functionality.
    - Developed Home page with a call-to-action for stock management.
    - Introduced AddArticle component for adding new stock items.
    - Created StockCreate page to facilitate article addition.
    - Implemented loading state for stock pages.
    - Developed Stock page to display a list of articles with navigation.
    - Defined Article type and NewArticle type for type safety.
    - Implemented articles database utility functions for fetching and adding articles.
    - Added sleep utility for simulating delays in API calls.
    - Configured TypeScript settings for the project.

---
+++ back/src/api.ts
@@ -0,0 +1,53 @@
+import { Router, type Request, type Response, json } from "express";
+import type { Article, NewArticle } from "./interfaces/article";
+
+const sleep = (delay: number) => new Promise((r) => setTimeout(r, delay));
+
+export const generateId = (): string => {
+  return Date.now() + "_" + Math.round(Math.random() * 1e12);
+};
+
+let articles: Article[] = [
+  { id: "a1", name: "Tournevis", price: 2.66, qty: 123 },
+  { id: "a2", name: "Marteau", price: 3, qty: 78 },
+];
+
+const app = Router();
+
+const date = (req: Request, res: Response) => {
+  res.json({ date: new Date() });
+};
+
+app.get("/date", date);
+
+app.use(async (req, res, next) => {
+  await sleep(1000);
+  next();
+});
+
+app.get("/articles", async (req, res) => {
+  res.json(articles);
+});
+
+app.use(json());
+
+app.post("/articles", (req, res) => {
+  const newArticle: NewArticle = req.body;
+  const article = { ...newArticle, id: generateId() };
+  articles.push(article);
+  res.status(201).end();
+});
+
+app.delete("/articles", (req, res) => {
+  const ids: string[] = req.body;
+  articles = articles.filter((a) => !ids.includes(a.id));
+  res.status(204).end();
+});
+
+app.delete("/articles/:id", (req, res) => {
+  const id = req.params.id;
+  articles = articles.filter((a) => a.id !== id);
+  res.status(204).end();
+});
+
+export default app;
---
+++ front/src/utils/articles-db.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { Article, NewArticle } from "@/types/Article";
+import { sleep } from "./sleep";
+
+const url = process.env.BACKEND_URL || "http://localhost:3333/api/articles";
+
+export const getArticles = async () => {
+  await sleep(2000);
+  console.log("url: ", url);
+  const response = await fetch(url, {
+    cache: "no-store",
+  });
+  const articles: Article[] = await response.json();
+  return articles;
+};
+
+export const addArticle = async (newArticle: NewArticle) => {
+  await sleep(2000);
+  await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(newArticle),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
