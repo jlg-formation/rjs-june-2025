3c21d559f9db273bcb59790af8a3f945ca350ac6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-24 14:02:39 +0100

    feat: update VSCode extensions and improve Legal page functionality with timer and counter

--- src/app/legal/page.tsx
+++ src/app/legal/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Legal() {
+  console.log("rendering Legal");
   const [dateTimeStr, setDateTimeStr] = useState("");
+  const [counter, setCounter] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
+
+  const timer = useRef<ReturnType<typeof setInterval> | undefined>(undefined);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      console.log("timer", dateTimeStr);
       setDateTimeStr(new Date().toLocaleString());
     }, 1000);
 
@@ -17,10 +21,40 @@ export default function Legal() {
     };
   }, []);
 
+  useEffect(() => {
+    if (isCounting) {
+      console.log("setInterval");
+      timer.current = setInterval(() => {
+        setCounter((c) => c + 1);
+      }, 1000);
+    } else if (timer.current) {
+      console.log("clearInterval isCounting false");
+      clearInterval(timer.current);
+      timer.current = undefined;
+    }
+
+    return () => {
+      console.log("housekeeping");
+      if (timer.current) {
+        console.log("clearInterval housekeeping");
+        clearInterval(timer.current);
+        timer.current = undefined;
+      }
+    };
+  }, [isCounting]);
+
+  const handleToggle = () => {
+    setIsCounting((isCounting) => !isCounting);
+  };
+
   return (
     <main className="main">
       <h1 className="title">Mentions Légales</h1>
       <p>{dateTimeStr}</p>
+      <button className="btn" onClick={handleToggle}>
+        counter = {counter}
+      </button>
+      <p>isCounting = {isCounting ? "true" : "false"}</p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
         quas necessitatibus. Nostrum tempora aperiam sunt ipsum quibusdam
