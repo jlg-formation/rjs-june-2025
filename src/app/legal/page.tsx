a1ccd233d89f7c924dcd2965c60885eb7d461419
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-24 11:37:17 +0100

    feat: add real-time date and time display to Legal page

--- src/app/legal/page.tsx
+++ src/app/legal/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Legal() {
+  const [dateTimeStr, setDateTimeStr] = useState("");
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log("timer", dateTimeStr);
+      setDateTimeStr(new Date().toLocaleString());
+    }, 1000);
+
+    return () => {
+      console.log("nettoyage");
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <main className="main">
       <h1 className="title">Mentions Légales</h1>
+      <p>{dateTimeStr}</p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
         quas necessitatibus. Nostrum tempora aperiam sunt ipsum quibusdam
